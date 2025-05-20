8f5375b5727bb5a8dd030ac3fa9e470741961f85
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-05-20 23:49:26 +0900

    feat: define data2pdb

--- src/lammps_utils/io/__init__.py
+++ src/lammps_utils/io/__init__.py
@@ -4,22 +4,25 @@ This module provides functions to convert LAMMPS data files to GROMACS gro files
 and to load data from LAMMPS data files
 """
 
-from ._convert import data2gro
+from ._convert import data2gro, data2pdb
 from ._load import (
     get_atom_type_masses,
     get_atom_type_symbols,
     get_cell_bounds,
     get_n_atom_types,
     get_n_atoms,
+    get_n_bonds,
     load_data,
 )
 
 __all__ = (
     "data2gro",
+    "data2pdb",
     "get_atom_type_symbols",
     "load_data",
     "get_cell_bounds",
     "get_n_atoms",
+    "get_n_bonds",
     "get_n_atom_types",
     "get_atom_type_masses",
 )
