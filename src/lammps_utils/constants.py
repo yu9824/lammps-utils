956ec5521b125ea2119b9178d534baef4a4732a4
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-05-20 12:07:05 +0900

    feat: define 'load_data'

---
+++ src/lammps_utils/constants.py
@@ -0,0 +1,24 @@
+from rdkit import Chem
+
+__all__ = ("N_ELEMENTS", "MAP_ELEMENT_MASSES", "COLS_DATA_DTYPE")
+
+N_ELEMENTS = 118
+"""The number of elements in the periodic table. The last element is Oganesson (Og)."""
+
+
+MAP_ELEMENT_MASSES: dict[str, float] = {
+    _atom.GetSymbol(): _atom.GetMass()
+    for _atom in (Chem.Atom(_i) for _i in range(1, N_ELEMENTS + 1))
+}
+"""A dictionary mapping element symbols to their atomic masses in amu (g/mol)."""
+
+COLS_DATA_DTYPE = {
+    "id": int,
+    "mol": int,
+    "type": int,
+    "charge": float,
+    "x": float,
+    "y": float,
+    "z": float,
+}
+"""A dictionary mapping column names to their data types in the LAMMPS data file."""
---
+++ src/lammps_utils/io/_load.py
@@ -0,0 +1,297 @@
+import io
+import os
+import re
+from pathlib import Path
+from typing import Literal, Union, overload
+
+import pandas as pd
+
+from lammps_utils.constants import COLS_DATA_DTYPE, MAP_ELEMENT_MASSES
+
+PATTERN_N_ATOMS = r"\s*(\d+)\s+atoms\s*\n"
+PATTERN_N_ATOM_TYPES = r"\s*(\d+)\s+atom types\s*\n"
+
+
+def _read_data_or_buffer(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+) -> str:
+    """
+    Read a LAMMPS data file or a file-like object and return its content as a string.
+    This function handles both file paths and file-like objects (e.g., StringIO).
+    If a file path is provided, it checks if the file exists and raises a FileNotFoundError
+
+    Parameters
+    ----------
+    filepath_data_or_buffer : Union[str, os.PathLike, io.TextIOBase]
+        The file path or file-like object to read.
+
+    Returns
+    -------
+    str
+        The content of the file or file-like object as a string.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the file path does not exist.
+    TypeError
+        If the input is neither a string, os.PathLike, nor a file-like object.
+    """
+    if isinstance(filepath_data_or_buffer, (str, os.PathLike)):
+        filepath_data_or_buffer = Path(filepath_data_or_buffer)
+        if not filepath_data_or_buffer.is_file():
+            raise FileNotFoundError(
+                f"File {filepath_data_or_buffer} does not exist."
+            )
+
+        with open(filepath_data_or_buffer, mode="r") as f:
+            return f.read()
+    elif isinstance(filepath_data_or_buffer, io.TextIOBase):
+        return filepath_data_or_buffer.read()
+    else:
+        raise TypeError(
+            f"Expected str, os.PathLike or io.TextIOBase, got {type(filepath_data_or_buffer)}"
+        )
+
+
+def get_n_atoms(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+) -> int:
+    """
+    Get the number of atoms from a LAMMPS data file or a file-like object.
+
+    Parameters
+    ----------
+    filepath_data_or_buffer : Union[str, os.PathLike, io.TextIOBase]
+        The file path or file-like object to read.
+
+    Returns
+    -------
+    int
+        The number of atoms in the LAMMPS data file.
+
+    Raises
+    ------
+    ValueError
+        If the number of atoms cannot be found in the file.
+    """
+    content = _read_data_or_buffer(filepath_data_or_buffer)
+    if _result_n_atoms := re.search(PATTERN_N_ATOMS, content):
+        return int(_result_n_atoms.group(1))
+    else:
+        raise ValueError(
+            f"Could not find number of atoms in {filepath_data_or_buffer}. "
+            f"Make sure the file is a valid LAMMPS data file."
+        )
+
+
+def get_n_atom_types(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+) -> int:
+    """
+    Get the number of atom types from a LAMMPS data file or a file-like object.
+
+    Parameters
+    ----------
+    filepath_data_or_buffer : Union[str, os.PathLike, io.TextIOBase]
+        The file path or file-like object to read.
+
+    Returns
+    -------
+    int
+        The number of atom types in the LAMMPS data file.
+
+    Raises
+    ------
+    ValueError
+        If the number of atom types cannot be found in the file.
+    """
+    content = _read_data_or_buffer(filepath_data_or_buffer)
+    if _result_n_atom_types := re.search(PATTERN_N_ATOM_TYPES, content):
+        return int(_result_n_atom_types.group(1))
+    else:
+        raise ValueError(
+            f"Could not find number of atom types in {filepath_data_or_buffer}. "
+            f"Make sure the file is a valid LAMMPS data file."
+        )
+
+
+def get_atom_type_masses(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+) -> dict[int, float]:
+    """
+    Get the masses of atom types from a LAMMPS data file or a file-like object.
+
+    Parameters
+    ----------
+    filepath_data_or_buffer : Union[str, os.PathLike, io.TextIOBase]
+        The file path or file-like object to read.
+
+    Returns
+    -------
+    dict[int, float]
+        A dictionary mapping atom type IDs to their masses.
+
+    Raises
+    ------
+    ValueError
+        If the atom type masses cannot be found in the file.
+    """
+    content = _read_data_or_buffer(filepath_data_or_buffer)
+    n_atom_types = get_n_atom_types(io.StringIO(content))
+    if _result_masses := re.search(
+        r"\s*Masses.*\n\s*" + r"(\d+)\s+([\d\.]+).*\n\s*" * n_atom_types,
+        content,
+    ):
+        return {
+            int(_result_masses.group(2 * _i - 1)): float(
+                _result_masses.group(2 * _i)
+            )
+            for _i in range(1, n_atom_types + 1)
+        }
+    else:
+        raise ValueError(
+            f"Could not find atom type masses in {filepath_data_or_buffer}. "
+            f"Make sure the file is a valid LAMMPS data file."
+        )
+
+
+def get_atom_type_symbols(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+) -> dict[int, str]:
+    """
+    Get the symbols of atom types from a LAMMPS data file or a file-like object.
+
+    Parameters
+    ----------
+    filepath_data_or_buffer : Union[str, os.PathLike, io.TextIOBase]
+        The file path or file-like object to read.
+
+    Returns
+    -------
+    dict[int, str]
+        A dictionary mapping atom type IDs to their symbols.
+    """
+    atom_type_masses = get_atom_type_masses(filepath_data_or_buffer)
+    sr_element_masses = pd.Series(MAP_ELEMENT_MASSES)
+    return {
+        _atom_id: sr_element_masses.index.tolist()[
+            (sr_element_masses - _mass).abs().argmin()
+        ]
+        for _atom_id, _mass in atom_type_masses.items()
+    }
+
+
+def get_cell_bounds(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+) -> tuple[tuple[float, float], tuple[float, float], tuple[float, float]]:
+    """
+    Get the cell limits from a LAMMPS data file or a file-like object.
+
+    Parameters
+    ----------
+    filepath_data_or_buffer : Union[str, os.PathLike, io.TextIOBase]
+        The file path or file-like object to read.
+
+    Returns
+    -------
+    tuple[tuple[float, float], tuple[float, float], tuple[float, float]]
+        A tuple containing the cell limits for x, y, and z axes.
+    """
+    content = _read_data_or_buffer(filepath_data_or_buffer)
+
+    _list_cell_bounds: list[tuple[float, float]] = []
+    for x in ("x", "y", "z"):
+        if _result_cell_bounds := re.search(
+            rf"\s*([-+\d\.e]+)\s+([-+\d\.e]+)\s+{x}lo\s+{x}hi", content
+        ):
+            _list_cell_bounds.append(
+                (
+                    float(_result_cell_bounds.group(1)),
+                    float(_result_cell_bounds.group(2)),
+                )
+            )
+        else:
+            raise ValueError(
+                f"Could not find {x} cell bounds in {filepath_data_or_buffer}. "
+                f"Make sure the file is a valid LAMMPS data file."
+            )
+    cell_bounds = tuple(_list_cell_bounds)
+    assert len(cell_bounds) == 3
+    return cell_bounds
+
+
+@overload
+def load_data(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+    return_cell_bounds: Literal[False] = False,
+) -> pd.DataFrame: ...
+
+
+@overload
+def load_data(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+    return_cell_bounds: Literal[True] = True,
+) -> tuple[
+    pd.DataFrame,
+    tuple[tuple[float, float], tuple[float, float], tuple[float, float]],
+]: ...
+
+
+def load_data(
+    filepath_data_or_buffer: Union[str, os.PathLike, io.TextIOBase],
+    return_cell_bounds: bool = False,
+) -> pd.DataFrame:
+    """
+    Load atom data from a LAMMPS data file or a file-like object into a DataFrame.
+
+    Parameters
+    ----------
+    filepath_data_or_buffer : Union[str, os.PathLike, io.TextIOBase]
+        The file path or file-like object to read.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame containing the atom data, with columns for ID, type, x, y, z, and symbol.
+
+    Raises
+    ------
+    ValueError
+        If the atom data cannot be found in the file.
+    """
+    content = _read_data_or_buffer(filepath_data_or_buffer)
+
+    n_atoms = get_n_atoms(io.StringIO(content))
+    atom_type_symbols = get_atom_type_symbols(io.StringIO(content))
+
+    if _result_atoms := re.search(
+        r"\s*Atoms.*?\n("
+        + r"\s*\d+\s+\d+\s+\d+\s+[-+\d\.e]+\s+[-+\d\.e]+\s+[-+\d\.e]+\s+[-+\d\.e]+.*\n"
+        * n_atoms
+        + r")",
+        content,
+    ):
+        _df_data = (
+            pd.read_table(
+                io.StringIO(_result_atoms.group(1)), header=None, sep="\\s+"
+            )
+            .iloc[:, : len(COLS_DATA_DTYPE)]
+            .rename(columns=dict(enumerate(COLS_DATA_DTYPE.keys())))
+            .astype(COLS_DATA_DTYPE)
+        )
+    else:
+        raise ValueError(
+            f"Could not find atom data in {filepath_data_or_buffer}. "
+            f"Make sure the file is a valid LAMMPS data file."
+        )
+
+    assert _df_data["type"].max() == get_n_atom_types(io.StringIO(content))
+    _df_data["symbol"] = _df_data["type"].replace(atom_type_symbols)
+
+    _df_data.set_index("id", inplace=True)
+    _df_data.sort_index(inplace=True)
+    if return_cell_bounds:
+        return _df_data, get_cell_bounds(io.StringIO(content))
+    else:
+        return _df_data
