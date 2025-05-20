92c65e40c50699bfbafac0569fe8c838382b03d4
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-05-20 12:07:33 +0900

    feat: define 'data2gro'

---
+++ src/lammps_utils/io/__init__.py
@@ -0,0 +1,19 @@
+from ._convert import data2gro
+from ._load import (
+    get_atom_type_masses,
+    get_atom_type_symbols,
+    get_cell_bounds,
+    get_n_atom_types,
+    get_n_atoms,
+    load_data,
+)
+
+__all__ = (
+    "data2gro",
+    "get_atom_type_symbols",
+    "load_data",
+    "get_cell_bounds",
+    "get_n_atoms",
+    "get_n_atom_types",
+    "get_atom_type_masses",
+)
