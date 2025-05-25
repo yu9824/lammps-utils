374ebda1da704c0609c51a7615f2661c40889b0e
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-05-25 19:53:24 +0900

    feat: Define find_main_chains method

---
+++ src/lammps_utils/graph/__init__.py
@@ -0,0 +1,3 @@
+from ._graph import farthest_node_pair, nodes_in_cycles
+
+__all__ = ("farthest_node_pair", "nodes_in_cycles")
--- src/lammps_utils/rdkit/__init__.py
+++ src/lammps_utils/rdkit/__init__.py
@@ -1,3 +1,4 @@
 from ._pbc import fix_molecule_periodic_boundary
+from ._poly import find_main_chains
 
-__all__ = ("fix_molecule_periodic_boundary",)
+__all__ = ("fix_molecule_periodic_boundary", "find_main_chains")
