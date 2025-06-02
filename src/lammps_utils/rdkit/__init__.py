3b13691aa266e7e4ec769efc1bf7db91342007e4
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-06-02 21:56:16 +0900

    refactor: move get_bond_order

--- src/lammps_utils/rdkit/__init__.py
+++ src/lammps_utils/rdkit/__init__.py
@@ -1,4 +1,4 @@
-from ._pbc import fix_molecule_periodic_boundary
+from ._pbc import unwrap_rdkit_mol_under_pbc
 from ._poly import find_main_chains
 
-__all__ = ("fix_molecule_periodic_boundary", "find_main_chains")
+__all__ = ("unwrap_rdkit_mol_under_pbc", "find_main_chains")
