93bac8900ee531c239ee2c12443a18e3bf032f84
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-06-03 23:12:06 +0900

    fix: compute_rg

--- src/lammps_utils/rdkit/_rg.py
+++ src/lammps_utils/rdkit/_rg.py
@@ -1,22 +1,40 @@
+from typing import Literal
+
 import numpy as np
 from rdkit import Chem
 
 
-def calculate_rg(mol: Chem.rdchem.Mol, confId: int = -1, mode="mass") -> float:
+def compute_rg(
+    mol: Chem.rdchem.Mol,
+    confId: int = -1,
+    mode: Literal["mass", "geometry"] = "mass",
+    removeHs: bool = False,
+) -> float:
     if mol.GetNumConformers() == 0:
         raise ValueError("No conformer")
+
+    if removeHs:
+        mol = Chem.RemoveHs(mol)
 
     conf = mol.GetConformer(confId)
     positions = conf.GetPositions()
 
     if mode == "mass":
-        weights = tuple(atom.GetMass() for atom in mol.GetAtoms())
+        weights = tuple(
+            atom.GetMass()
+            for atom in mol.GetAtoms()
+            if isinstance(atom, Chem.Atom)
+        )
     elif mode == "geometry":
         weights = None
     else:
         raise ValueError
 
     center = np.average(positions, axis=0, weights=weights, keepdims=True)
-    return np.square(
-        np.average(np.square(positions - center), axis=0, weights=weights)
+    return np.sqrt(
+        np.average(
+            np.sum(np.square(positions - center), axis=1),
+            axis=0,
+            weights=weights,
+        )
     ).item()
