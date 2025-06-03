0d17bab296f322bdf597ea43f89460406607540a
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-06-03 23:13:43 +0900

    refactor: Update import path

--- src/lammps_utils/rdkit/__init__.py
+++ src/lammps_utils/rdkit/__init__.py
@@ -10,5 +10,11 @@ This submodule includes:
 from ._bond import get_bond_order
 from ._pbc import unwrap_rdkit_mol_under_pbc
 from ._poly import find_main_chains
+from ._rg import compute_rg
 
-__all__ = ("get_bond_order", "unwrap_rdkit_mol_under_pbc", "find_main_chains")
+__all__ = (
+    "get_bond_order",
+    "unwrap_rdkit_mol_under_pbc",
+    "find_main_chains",
+    "compute_rg",
+)
