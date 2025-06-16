c3da0725e14ca7a2394d170acad10850baed6949
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-06-16 22:11:13 +0900

    feat: Define new method; generate_conformer

---
+++ src/lammps_utils/chem/conformer/__init__.py
@@ -0,0 +1,11 @@
+from ._generate import (
+    generate_conformers_from_smiles,
+    generate_minimized_conformer,
+    minimize_conformer,
+)
+
+__all__ = (
+    "generate_conformers_from_smiles",
+    "generate_minimized_conformer",
+    "minimize_conformer",
+)
