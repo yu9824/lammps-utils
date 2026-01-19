409a5ca87168fa65f8197f9bfe89f4a9548f308b
yu9824 <58211916+yu9824@users.noreply.github.com>
2026-01-19 21:57:28 +0900

    feat: polymerize_linear

--- src/lammps_utils/chem/polymer/__init__.py
+++ src/lammps_utils/chem/polymer/__init__.py
@@ -1,4 +1,10 @@
+from ._builder import polymerize_linear
 from ._connect import connect_mols, detect_head_and_tail
 from ._polymer import find_main_chains
 
-__all__ = ("connect_mols", "detect_head_and_tail", "find_main_chains")
+__all__ = (
+    "connect_mols",
+    "detect_head_and_tail",
+    "find_main_chains",
+    "polymerize_linear",
+)
