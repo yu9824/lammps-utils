e98a8eaa794afdfd1d97de2d6686a1a935e2cb18
yu9824 <58211916+yu9824@users.noreply.github.com>
2026-01-22 07:25:44 +0900

    chore: Update __init__.py

--- src/lammps_utils/io/mol/__init__.py
+++ src/lammps_utils/io/mol/__init__.py
@@ -1,10 +1,16 @@
-from ._mol import MolFromLAMMPSData, MolFromLAMMPSDump, MolToMol2Block
+from ._mol import (
+    MolFromLAMMPSData,
+    MolFromLAMMPSDump,
+    MolToMol2Block,
+    MolToMol2File,
+)
 from ._pbc import unwrap_mol_under_pbc, wrap_mol_into_cell
 
 __all__ = (
     "MolFromLAMMPSData",
     "MolFromLAMMPSDump",
     "MolToMol2Block",
+    "MolToMol2File",
     "unwrap_mol_under_pbc",
     "wrap_mol_into_cell",
 )
