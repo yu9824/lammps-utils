b1df1776ab9d3db9043d0670fa83db319add4a9a
yu9824 <58211916+yu9824@users.noreply.github.com>
2026-02-05 07:19:38 +0900

    refactor: Move view_3d to .visualize

--- src/lammps_utils/chem/conformer/__init__.py
+++ src/lammps_utils/chem/conformer/__init__.py
@@ -3,11 +3,9 @@ from ._generate import (
     generate_minimized_conformer,
     minimize_conformer,
 )
-from ._view import view_3d
 
 __all__ = (
     "generate_conformers_from_smiles",
     "generate_minimized_conformer",
     "minimize_conformer",
-    "view_3d",
 )
